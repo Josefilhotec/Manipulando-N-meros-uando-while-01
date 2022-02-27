fe1a632810ab9d4051f3efa5065a768f0d7e926e
Jose Rodrigues <99560762+Josefilhotec@users.noreply.github.com>
2022-02-27 14:21:38 -0300

    Update Manipulando Números.py

--- Manipulando Números.py
+++ Manipulando Números.py
@@ -1,4 +1,4 @@
-# programa que leier vários números inteiros pelo teclado.
+# programa que ler vários números inteiros pelo teclado.
 # No final da execução, mostre a soma e a média entre todos os valores e qual foi o maior e o menor valores lido.
 # O programa deve perguntar ao usuário se ele quer ou não continuar a digitar valores.
 from time import sleep
